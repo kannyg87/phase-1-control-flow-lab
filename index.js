879e347c1c9a0f1c78785f60a99b377834e364de
Kanny <kanny.ghafour87@gmail.com>
2023-07-06 23:45:15 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(val){
   // Write your code here!
+ if (val<= 400){
+  return "This one is on me!"
+ }
+ if (val>= 400 && val <=2000){
+  return "That will be twenty bucks."
+ }
+ if (val>= 2000 &&val < 2500){
+  return "I will gladly take your thirty bucks."
+ }
+ if (val >= 2500) {
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+    switch(tip) {
+        case 'generous' : return 'Thank you so much.';
+        case 'not as generous' : return 'Thank you.';
+
+        default : return 'Bye.';
+
+    }
 }
